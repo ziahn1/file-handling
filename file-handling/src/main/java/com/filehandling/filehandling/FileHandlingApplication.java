e9e1eaa3002129a0c112486c62d4448f4981644b
zia.haider <86730092+zianhaidernaqvi@users.noreply.github.com>
2023-01-05 15:45:47 +0530

    Initial commit

---
+++ file-handling/src/main/java/com/filehandling/filehandling/FileHandlingApplication.java
@@ -0,0 +1,15 @@
+package com.filehandling.filehandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileHandlingApplication.class, args);
+
+		
+	}
+
+}
---
+++ file-handling/src/test/java/com/filehandling/filehandling/FileHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filehandling.filehandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
